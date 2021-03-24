a48aa1e99b192e9fb5a75ffcc2be0314e775086d
Krutilin Sergey <krutilin.sergey.ks@gmail.com>
2021-03-24 10:08:21 +0300

    set-up angular material

---
+++ src/app/modules/shared/shared.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { MatInputModule } from '@angular/material/input';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatListModule,
+    MatIconModule,
+    DragDropModule,
+    MatInputModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+  ]
+})
+export class SharedModule { }
