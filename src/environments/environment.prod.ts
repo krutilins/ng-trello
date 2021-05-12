8526f37db90dfee89216bebbf036feeefb54cfe9
Krutilin Sergey <krutilin.sergey.ks@gmail.com>
2021-05-12 09:46:21 +0300

    update prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: true,
+  production: false,
   firebase: {
     apiKey: 'AIzaSyAdeT0MeEke_ARCcS19YyIs9D4FmTmKqqM',
     authDomain: 'ng-trello-c4f4d.firebaseapp.com',
@@ -7,5 +7,18 @@ export const environment = {
     storageBucket: 'ng-trello-c4f4d.appspot.com',
     messagingSenderId: '904467297110',
     appId: '1:904467297110:web:baa09d3c34d90851bc5cf7'
+  },
+  vk: {
+    oauth: {
+      apiUrl: 'https://oauth.vk.com/authorize',
+      redirectUri: 'http://localhost:4200/auth/vk',
+      responceType: 'token'
+    },
+    api: {
+      apiUrl: 'https://api.vk.com/method',
+      usersGetEndpoint: '/users.get',
+    },
+    clientId: '7847226',
+    apiVersion: ''
   }
 };
